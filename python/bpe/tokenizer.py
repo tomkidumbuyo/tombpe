3c8676bdfae9f954c8286ce52daa86b5e6425e83
tomkidumbuyo <mryoungtommy@gmail.com>
2025-09-02 10:21:21 +0300

    Fixed missing characters handling

--- python/bpe/tokenizer.py
+++ python/bpe/tokenizer.py
@@ -1,46 +1,71 @@
 from pathlib import Path
 from collections import Counter
 import json
+import regex as re 
+from collections import defaultdict
+from itertools import combinations
 
 class BasicTokenizer:
+
+    PRE_TOKENIZATION_REGEX = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
     
     def __init__(self, vocab_size=32768, min_frequency=2):
         self.vocab_size = vocab_size
         self.min_frequency = min_frequency
         self.paired_tokens = {}
+        self.utf8_vocab = {i: b.decode('utf-8', errors='ignore') for i, b in ((i, bytes([i])) for i in range(256))}
 
-    def _get_pair_frequencies(self, tokens, min_frequency=1) -> dict[tuple, int]:
-        pair_frequencies = dict(Counter(zip(tokens, tokens[1:])))
-        return {pair: count for pair, count in pair_frequencies.items() if count >= min_frequency}
+    def _get_pair_frequencies(self, divided_tokens: list[list[int]]) -> dict[tuple, int]:
+        result = defaultdict(int)
+        for sublist in divided_tokens:
+            for i in range(len(sublist) - 1):
+                pair = (sublist[i], sublist[i + 1])
+                result[pair] += 1
+        return {k: v for k, v in result.items() if v >= self.min_frequency}
 
     def _add_new_paired_tokens(self, pair: str) -> int:
-        newPairedTokenId = len(self.paired_tokens) + 256
-        if(pair[0] >= 256): pair = (*self.paired_tokens[pair[0]], pair[1])
-        if(pair[len(pair) - 1] >= 256): pair = (*pair[:-1], *self.paired_tokens[pair[len(pair) - 1]])
+        newPairedTokenId = len(self.utf8_vocab) + len(self.paired_tokens) + 1
+
+        if(pair[0] > len(self.utf8_vocab)): pair = (*self.paired_tokens[pair[0]], pair[1])
+        if(pair[len(pair) - 1] > len(self.utf8_vocab)): pair = (*pair[:-1], *self.paired_tokens[pair[len(pair) - 1]])
+
         key = next((k for k, v in self.paired_tokens.items() if v == pair), None)
+
         if key: return key
         self.paired_tokens[newPairedTokenId] = pair
         return newPairedTokenId
 
-    def _merge_pairs(self, tokens):
+    def _merge_pairs(self, divided_tokens):
         while True:
-            pair_frequencies = self._get_pair_frequencies(tokens, 2)
+            pair_frequencies = self._get_pair_frequencies(divided_tokens)
             if len(pair_frequencies) == 0:
                 break
             i = 0
             for pair, count in pair_frequencies.items():
                 newId = self._add_new_paired_tokens(pair)
-                while i < len(tokens) - 1:
-                    if tokens[i] == pair[0] and tokens[i + 1] == pair[1]:
-                        tokens[i:i+2] = [newId]
-                    else:
-                        i += 1 
-        return tokens
+                for i in range(len(divided_tokens)):
+                    j = len(divided_tokens[i]) - 2
+                    while j >= 0:
+                        if divided_tokens[i][j] == pair[0] and divided_tokens[i][j + 1] == pair[1]:
+                            divided_tokens[i][j:j+2] = [newId]
+                        j -= 1
+                        
+        all_tokens = [item for sublist in divided_tokens for item in sublist]
+        self.paired_tokens = {k: v for k, v in self.paired_tokens.items() if k in all_tokens}            
+        return all_tokens
 
-
+    def _add_unique_characters_to_utf8_vocab(self, text: str):
+        invalid_chars = set(text) - {chr(i) for i in range(256)}
+        for invalid_char in invalid_chars:
+            self.utf8_vocab[len(self.utf8_vocab) + 1] = invalid_char
+   
     def train(self, training_string):
+        training_string_sections = self.PRE_TOKENIZATION_REGEX.findall(training_string)
+        self._get_unique_characters(training_string)
         tokens = list(training_string.encode("utf-8"))
-        mergedTokens = self._merge_pairs(tokens)
+        mergedTokens = self._merge_pairs([list(section.encode("utf-8")) for section in training_string_sections])
+        self.paired_tokens = {**self.paired_tokens, **self.paired_tokens}
+
         self._get_vocab_from_paired_tokens()
 
     def encode(self, text: str) -> list[int]:
@@ -61,16 +86,13 @@ class BasicTokenizer:
         return result
 
     def decode(self, tokens: list[int]) -> str:
-        print(tokens)
         return ''.join([self.vocab[token] for token in tokens])
 
     def _get_vocab_from_paired_tokens(self):
-        self.vocab = {i: b.decode('utf-8', errors='ignore') 
-              for i, b in ((i, bytes([i])) for i in range(256)) 
-              if b.decode('utf-8', errors='ignore') != ''}
+        self.vocab = self.utf8_vocab
         for token_id, pair in self.paired_tokens.items():
             self.vocab[token_id] = ''.join([self.vocab[p] for p in pair])
-        self.vocab = dict(sorted(self.vocab.items(), key=lambda x: len(x[1]), reverse=True))
+        self.vocab = dict(sorted(((k, v) for k, v in self.vocab.items() if v != ""), key=lambda x: len(x[1]), reverse=True))
         return self.vocab
 
     def save(self, output_path: str):
--- python/test/test_tokenization.py
+++ python/test/test_tokenization.py
@@ -2,8 +2,11 @@ from bpe.tokenizer import BasicTokenizer
 
 if __name__ == "__main__":
     tokenizer = BasicTokenizer(vocab_size=32768)
-    training_data = "abcabcabcabc"
+
+    with open("test\short-text-file.txt", "r", encoding="utf-8") as f:
+        training_data = f.read()
     tokenizer.train(training_data)
+    tokenizer.save("vocab2.json")
     tokenized_string = tokenizer.encode(training_data)
 
     print("Tokenized string:", tokenized_string)
@@ -11,5 +14,5 @@ if __name__ == "__main__":
     detokenized_string = tokenizer.decode(tokenized_string)
 
     print("Detokenized string:", detokenized_string)
-    tokenizer.save("vocab.json")
+    
     
